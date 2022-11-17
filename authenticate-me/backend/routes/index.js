f651f350e252e78324c1292f9acdd3fc9723a7ba
Chino <111507103+Me-legna@users.noreply.github.com>
2022-11-17 00:16:26 -0500

    Add an API router

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -15,5 +15,9 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
-  
+
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 module.exports = router;
